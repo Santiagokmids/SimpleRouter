504033561b7881c3158c53cc226f25ef47644731
SANTIAGO TROCHEZ VELASCO <1193573597@u.icesi.edu.co>
2022-02-11 14:13:03 -0500

    Function home has been added

--- Router.js
+++ Router.js
@@ -1,1 +1,16 @@
-let routes = {};let templates = {};let app_div = document.getElementById('app');
+let routes = {};
+let templates = {};
+
+let app_div = document.getElementById('app');
+
+function home(){
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/about';
+    link.innerText = 'About';
+
+    div.innerHTML = <h1>Home</h1>;
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+};
